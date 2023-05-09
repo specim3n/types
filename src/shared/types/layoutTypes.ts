c0d6a940eee3c857016dfac1dc78c9a05864cddb
Olivier Bossel <olivier.bossel@gmail.com>
2023-05-09 12:59:26 +0200

    update layout specs

--- src/shared/types/layoutTypes.ts
+++ src/shared/types/layoutTypes.ts
@@ -1,22 +1,17 @@
 import type { ISSpec, ISSpecData } from './specTypes';
 
 export interface ISLayoutData extends ISSpecData {
-    layout: ISLayoutDataLayout;
-    media: Record<string, ISLayoutDataLayout>;
-    cells: string[];
+    container: boolean;
+    media: Record<string, string>;
+    areas: Record<string, string>;
     id?: string;
     frontspec?: any;
     gap?: string;
 }
 
-export interface ISLayoutDataLayout {
-    id: string;
-    layout: string;
-}
-
 export interface ISLayoutSpec extends ISSpec {
     layouts: ISLayoutSpecLayout[];
-    custom?: boolean;
+    // custom?: boolean; // not supported for now...
 }
 
 export interface ISLayoutSpecLayout {
